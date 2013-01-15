89bc24b06d4dbb754234a26579181b6b154286ed
geauxvirtual <justin.guidroz@gmail.com>
2013-01-15 13:40:33 -0600

    Initial params.pp configuration

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,1 +1,17 @@
-require 'puppetlabs_spec_helper/rake_tasks'
+# Class: java::params
+#
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+#
+class java::params {
+  $version    = '1.7.0_11'
+  $source     = 'http://repo.vmop.local/java'
+  $source_rpm = 'jre-7u11-linux-x64.rpm'
+}
